09f5d1b49f2f90ff04bba60ac759dbb1c0b0d727
sriprada346 <sriprada346@gmail.com>
2023-06-26 21:56:28 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@
 import './App.css';
 import api from './api/axiosConfig';
 import {useState,useEffect} from 'react'; /*hook*/
+import Layout from './component/Layout';
+import {Routes,Route} from 'react-router-dom';
+import Home from './component/home/Home';
 
 function App() {
 
   const [movies,setMovies]=useState(); 
+  const [page,setPage] = useState(false);
+
   const getMovies = async () =>{
 
     try
     {
-      const response = await api.get("/api/vl/movies"); //mangaing thread functionality on client
+      console.log("hello it came here");
+      const response = await api.get('/api/vl/movies'); //mangaing thread functionality on client
       console.log('this is reponse.data!!! : ',response.data);
       setMovies(response.data);
     }
@@ -26,7 +32,15 @@ function App() {
 
   return (
     <div className="App">
-   <p>helo React</p>
+
+      <Routes>
+          <Route path="/" element={<Layout/>}>
+              <Route path="/" element={<Home movies={movies}/>}></Route>
+          </Route>
+      </Routes>
+
+
+   
     </div>
   );
 }
--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -13,7 +13,16 @@ import axios from 'axios';
  */
 
 const api= axios.create({
-  baseURL:'https://d95e-2409-408c-8d12-f082-64f3-61fd-d61c-e6f6.ngrok-free.app',
+  /*baseURL:'https://utor.serveo.net/api/vl/movies',*/
+  /*baseURL:'https://1290-2409-408c-8e92-db5c-42b-b9be-f735-2d34.ngrok-free.app',*/
+ baseURL:'http://localhost:8080',
+  withCredentials: false,
+  headers: {
+    
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', // Add the allowed HTTP methods
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Add any necessary headers
+  },
+  
 });
   export default api;
   
